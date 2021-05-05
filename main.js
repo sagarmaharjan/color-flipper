b265f317080c19e4a843951bd71810b38068560e
sagar.maharjan <sagar.maharjan@f1soft.com>
2021-05-05 08:12:55 +0545

    Code refinement and trying to code with class

--- main.js
+++ main.js
@@ -1,47 +1,61 @@
 'use strict';
 
-const main = document.querySelector('#main');
-const button = document.querySelector('button');
-const display = document.querySelector('#hex');
-const title = document.querySelector('h1');
-const nav = document.querySelector('nav');
+(function() {
+    const main = document.querySelector('#main');
+    const button = document.querySelector('button');
+    const display = document.querySelector('#hex');
+    const title = document.querySelector('h1');
+    const nav = document.querySelector('nav');
+    class ColorFlipper {
+        constructor() {
+            this.simple = ["green", "red", "rgba(133,122,200)", "#f15025"];
+            this.hex = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            this.type = ['#simple', '#hex'];
+            this.selectedType = this.type[0];
+            this.defaultColorCode = '#49A6E9';
+        }
 
-const simple = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const hex = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const type = ['#simple', '#hex'];
-let selectedType = type[0];
+        generateColorCode() {
+            if(this.selectedType === '#hex') {
+                const randomHexCode = Array.from({length: 6}, () => this.hex[Math.floor(Math.random() * 15)]);
+                return '#' + randomHexCode.join('');
+            }
+        
+            return this.simple[Array.from({length: 1}, () => Math.floor(Math.random() * 3))]
+        }
 
-function generateHexCode() {
-    if(selectedType === '#hex') {
-        const randomHexCode = Array.from({length: 6}, () => hex[Math.floor(Math.random() * 15)]);
-        return '#' + randomHexCode.join('');
+        changeColorOfElement(colorCode, ...selectors) {
+            selectors.forEach((selector) => {
+                if(selector.background) {
+                    selector.dom.style.backgroundColor = colorCode;
+                } else if(selector.color) {
+                    selector.dom.style.color = colorCode;
+                }
+            })
+        }f
     }
 
-    return simple[Array.from({length: 1}, () => Math.floor(Math.random() * 3))]
-}
-
-function changeMainBackgroundColor(hexCode) {
-    main.style.backgroundColor = hexCode;
-    display.style.color = hexCode;
-    title.style.color = hexCode;
-}
+    let colorFlipper = new ColorFlipper();    
 
     button.addEventListener('click', () => {
-        const hexCode = generateHexCode();
-        changeMainBackgroundColor(hexCode);
-        display.textContent = hexCode;
+        const colorCode = colorFlipper.generateColorCode();
+        colorFlipper.changeColorOfElement(colorCode, {dom: main, background: true}, {dom: title, color: true});
+        display.textContent = colorCode;
     })
 
-nav.addEventListener('click', (event) => {
-    
-    if(event.target.tagName.toLowerCase() === 'a' ) {
-        if(event.target.parentElement.nextElementSibling === null) {
-            event.target.parentElement.previousElementSibling.children[0].classList.remove('active');
-        } else {
-            event.target.parentElement.nextElementSibling.children[0].classList.remove('active');
+    nav.addEventListener('click', (event) => {    
+        if(event.target.tagName.toLowerCase() === 'a' ) {
+            event.preventDefault();
+            colorFlipper.changeColorOfElement(colorFlipper.defaultColorCode, {dom: main, background: true}, {dom: title, color: true});
+            display.textContent = colorFlipper.defaultColorCode;
+            if(event.target.parentElement.nextElementSibling === null) {
+                event.target.parentElement.previousElementSibling.children[0].classList.remove('active');
+            } else {
+                event.target.parentElement.nextElementSibling.children[0].classList.remove('active');
+            }
+            event.target.classList.add('active');
+            
+            colorFlipper.selectedType = event.target.getAttribute('href');
         }
-        event.target.classList.add('active');
-        
-        selectedType = event.target.getAttribute('href');
-    }
-})
+    })
+})();
